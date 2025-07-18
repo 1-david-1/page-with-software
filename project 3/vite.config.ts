4d2a1e32f5d38619509ab613cb03bf83faee33f6
1-david-1 <davidmatuttis@gmail.com>
2025-07-18 16:39:12 +0200

    Update vite.config.ts

--- project 3/vite.config.ts
+++ project 3/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/page-with-security-planning/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
